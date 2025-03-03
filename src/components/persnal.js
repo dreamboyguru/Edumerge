752c3c1060b90e89e5c1a8f4c414965344d41cba
dreamboyguru <guruhanamannavar870@gmail.com>
2025-03-03 22:19:52 +0530

    Guru.

---
+++ src/components/persnal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const persnal = () => {
+  return (
+    <div>
+        stage.edumerge.com = emteam - 6Qf2pqges76eUupL3yV8DVv332MA3J1L
+        root = 7V9eck#feP4r
+
+        enhance sheet = https://docs.google.com/spreadsheets/d/1DWtJ2KceE_sbTUG-wweWOQH8CkJSNpV-K4zr3SlBavE/edit?gid=0#gid=0
+        bookmark Data = 
+        https://app.edumerge.com/V2/feereceipt/RazorpayJson/EM5018_Sep-24.json
+        https://app.edumerge.com/V2/feereceipt/CCAvenue/LogFiles/EM5436.json
+
+    </div>
+  )
+}
+
+export default persnal
